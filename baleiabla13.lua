fde2d85f20e470704defb9993273bb2159b9c536
soucrowley <crowleybaleia@gmail.com>
2026-09-20 18:38:09 -0300

    Create new file baleiabla13.lua

--- baleiabla13.lua
+++ baleiabla13.lua
@@ -7279,8 +7279,9 @@ local Library do
                     AnchorPoint = Vector2New(1, 0.5),
                     BorderSizePixel = 0,
                     Position = UDim2New(0, -16, 0.5, 0),
-                    Size = UDim2New(0, 55, 1, 0),
+                    Size = UDim2New(0, 55, 0, 0),
                     ZIndex = 2,
+                    AutomaticSize = Enum.AutomaticSize.Y,
                     BackgroundColor3 = FromRGB(16, 18, 21)
                 })  Items["Pages"]:AddToTheme({BackgroundColor3 = "Background"})
 
