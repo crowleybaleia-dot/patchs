270245c852f27577467187b3257b9a8f70966365
soucrowley <crowleybaleia@gmail.com>
2026-09-20 17:47:24 -0300

    Create new file baleiabla3.lua

--- baleiabla3.lua
+++ baleiabla3.lua
@@ -7279,9 +7279,9 @@ local Library do
                     AnchorPoint = Vector2New(1, 0.5),
                     BorderSizePixel = 0,
                     Position = UDim2New(0, -8, 0.5, 0),
-                    Size = UDim2New(0, 0, 1, 0),
+                    Size = UDim2New(0, 0, 0, 0),
                     ZIndex = 2,
-                    AutomaticSize = Enum.AutomaticSize.X,
+                    AutomaticSize = Enum.AutomaticSize.XY,
                     BackgroundColor3 = FromRGB(16, 18, 21)
                 })  Items["Pages"]:AddToTheme({BackgroundColor3 = "Background"})
 
@@ -7295,8 +7295,8 @@ local Library do
                     Parent = Items["Pages"].Instance,
                     Name = "\0",
                     BackgroundTransparency = 1,
-                    Size = UDim2New(0, 0, 1, 0),
-                    AutomaticSize = Enum.AutomaticSize.X,
+                    Size = UDim2New(0, 0, 0, 0),
+                    AutomaticSize = Enum.AutomaticSize.XY,
                     BorderColor3 = FromRGB(0, 0, 0),
                     BorderSizePixel = 0,
                     ScrollBarThickness = 0,
